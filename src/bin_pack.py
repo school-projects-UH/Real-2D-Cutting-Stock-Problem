348f0f9ebe75d3ff172caae5d11c43c93447a834
Tonito <otano0398@gmail.com>
2020-10-19 14:07:26 -0700

    Included the case we need to rotate the rectangle when finding the free rectangle that best fits

--- src/bin_pack.py
+++ src/bin_pack.py
@@ -1,14 +1,19 @@
 from classes import *
 
 def find_best_fit(rectangle, free_rectangles):
-    best_fit = (1000000, -1)
+    best_fit = (1000000, -1, False)
     for i in range(len(free_rectangles)):
         free_rect = free_rectangles[i]
         if rectangle.width <= free_rect.width and rectangle.height <= free_rect.height:
             shortest_side_fit = min(free_rect.width - rectangle.width, free_rect.height - rectangle.height)
-            if shortest_side_fit <= best_fit[0]:
-                best_fit = (shortest_side_fit, i)
-    return best_fit[1]
+            if shortest_side_fit < best_fit[0]:
+                best_fit = (shortest_side_fit, i, False)
+        # Try by rotating the rectangle
+        elif rectangle.width <= free_rect.height and rectangle.height <= free_rect.width:
+            shortest_side_fit = min(free_rect.width - rectangle.height, free_rect.height - rectangle.width)
+            if shortest_side_fit < best_fit[0]:
+                best_fit = (shortest_side_fit, i, True)
+    return best_fit[1], best_fit[2]
 
 
 
