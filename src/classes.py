42f3c77771b8155a2a82afad9c6b3783e2cdb681
Tonito <otano0398@gmail.com>
2020-10-19 15:54:23 -0700

    Fixed errors and added the __str__ methods

--- src/classes.py
+++ src/classes.py
@@ -4,15 +4,25 @@ class Rectangle:
         self.width = width
         self.height = height
 
+    def __str__(self):
+        return f'<Rectangle> width: {self.width}, height: {self.height}'
+
+
 '''A type of rectangular image requested by the client'''
 class Image(Rectangle):
-    def __init__(width, height, demand):
-        self.super(width, height)
+    def __init__(self, width, height, demand):
+        super().__init__(width, height)
         self.demand = demand
 
+    def __str__(self):
+        return f'<Image> width: {self.width}, height: {self.height}, demand: {self.demand}'
+
 '''An image placed on a sheet'''
-class PlacedImage(Image):
-    def __init__(width, height, position, rotated):
-        self.super(width, height)
+class PlacedImage(Rectangle):
+    def __init__(self, width, height, position, rotated):
+        super().__init__(width, height)
         self.position = position
         self.rotated = rotated
+
+    def __str__(self):
+        return f'<PlacedImage> width: {self.width}, height: {self.height}, position: {self.position}, rotated: {self.rotated}'
