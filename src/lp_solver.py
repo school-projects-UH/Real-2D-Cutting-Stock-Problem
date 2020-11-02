659aaf4387f0e5036912c727a6b28283d2f01dab
Ginrod <gilbecubano@gmail.com>
2020-11-01 21:43:26 -0500

    using cvxopt and glpk to solve LP problem

--- src/lp_solver.py
+++ src/lp_solver.py
@@ -1,40 +1,20 @@
-from pulp import LpMinimize, LpProblem, LpStatus, lpSum, LpVariable, PULP_CBC_CMD
+from cvxopt import matrix, solvers
 
 def solve_LP(bins, sheets_per_pattern, sheets):
-    '''
-    min * sum(xj* rj) 0 <= j < m
-    s.a
-    sum(pji * xj) >= di
-    xj >= 0
-    xj Integer
-    '''
-    waste = [b.free_area for b in bins]
-    demands = [s.demand for s in sheets]
-    areas = [s.width * s.height for s in sheets]
-    m, n = len(waste), len(demands)
-    model = LpProblem(name="cuts", sense=LpMinimize)
-
-    x = { j: LpVariable(f'x{j}', lowBound=0, cat="Integer") for j in range(m)}
-
+    w = [b.free_area for b in bins]
+    d = [s.demand for s in sheets]
+    a = [s.width * s.height for s in sheets]
+    m, n = len(w), len(d)
     p = [[0 for _ in range(n)] for _ in range(m)]
     for (j, i), sheets in sheets_per_pattern.items():
         p[j][i] = sheets
 
-    def overProduction(j):
-        return lpSum(p[i][j] * x[i] * areas[j] for i in range(m)) - demands[j] * areas[j]
+    c = matrix([float(wj + sum([p[j][i]*ai for i, ai in enumerate(a)])) for j, wj in enumerate(w)])
+    A = matrix([[float(-p[j][i]) for i in range(n)] for j in range(m)])
+    b = matrix([float(-di) for di in d])
 
-    def total_overProduction():
-        return lpSum(overProduction(j) for j in range(n))
+    sol = solvers.lp(c, A, b, solver='glpk')
+    fitness = sol['primal objective'] 
+    fitness = fitness and fitness - sum([d[i]*a[i] for i in range(n)]) or 0
 
-    for i, di in enumerate(demands):
-        model += (lpSum(pj[i] * x[j] for j, pj in enumerate(p)) >= di, f'constrain-{i}')
-
-    model += lpSum([lpSum(x[j] * waste[j] for j in range(m)), total_overProduction()])
-
-    # Solve the optimization problem
-    model.solve(solver=PULP_CBC_CMD(msg=0))
-
-    fitness = model.objective.value()
-    if fitness == None:
-        fitness = 0
-    return fitness, {variable.name: variable.varValue for variable in model.variables()}
+    return fitness, list(sol['x'])
