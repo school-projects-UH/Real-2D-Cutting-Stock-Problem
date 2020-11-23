529fcde3bbee2b70396920b769e0d9dfa95cbb4c
Tonito <otano0398@gmail.com>
2020-11-23 15:07:08 -0600

    Results of comparisson added

--- src/long_test.py
+++ src/long_test.py
@@ -28,7 +28,7 @@ class SolverConfig:
         self.worst_time = max(time, self.worst_time)
         self.worst_fitness = max(fitness, self.worst_fitness)
 
-sover_config_list = [{}, {
+sover_config_list = [{},  {
         'hill_climbing_neighbors': 30,
         'no_generations': 35,
         'roulette_pop': 60
@@ -37,6 +37,14 @@ sover_config_list = [{}, {
         'no_generations': 45,
         'roulette_pop': 60,
         'pop_size': 45
+        }, {
+            'prob_crossover': 0.90
+        }, {
+            'prob_crossover': 0.60
+        }, 
+        {'roulette_pop': 60},
+        {
+            'roulette_pop': 30
         }]
 sover_config_list = [SolverConfig(f'SolverNo{i + 1}', sover_config_list[i]) for i in range(len(sover_config_list))]
 
@@ -61,30 +69,30 @@ for i in range(no_tests):
     test_info += f'{main_sheet_width} {main_sheet_height}\n'
     test_info += '\n'.join([f'{sheet.width} {sheet.height} {sheet.demand}' for sheet in sheets]) + '\n'
     print(test_info)
-    with open(f'tests/collection/test{i + 1}.txt', "w") as input_file:
+    with open(f'tests/test{i + 1}.txt', "w") as input_file:
         input_file.write('\n'.join(test_info.split('\n')[1:-1]))
 
     for solver_config in sover_config_list:
         print(f"\nCalling {solver_config.name}...\n")
-        solver_config.call_solver(main_sheet, sheets, output=f'tests/collection/results/testNo{i + 1}_{solver_config.name}.txt')
+        solver_config.call_solver(main_sheet, sheets, output=f'tests/results/testNo{i + 1}_{solver_config.name}.txt')
 
 sorted_by_fitness = sorted(sover_config_list, key=lambda sc: sc.total_fitness)
 sorted_by_time = sorted(sover_config_list, key=lambda sc: sc.total_time)
 sorted_by_worst_fitness = sorted(sover_config_list, key=lambda sc: sc.worst_fitness)
 sorted_by_worst_time = sorted(sover_config_list, key=lambda sc: sc.worst_time)
 
-results_by_fitness = open('tests/collection/results/results_by_fitness.txt', "w")
+results_by_fitness = open('tests/results/results_by_fitness.txt', "w")
 for i, result in enumerate(sorted_by_fitness):
     results_by_fitness.write(f'{i + 1}- {result.name, result.total_fitness / sorted_by_fitness[0].total_fitness}\n')
 
-results_by_time = open('tests/collection/results/results_by_time.txt', "w")
+results_by_time = open('tests/results/results_by_time.txt', "w")
 for i, result in enumerate(sorted_by_time):
     results_by_time.write(f'{i + 1}- {result.name, result.total_time / no_tests}\n')
 
-results_by_worst_fitness = open('tests/collection/results/results_by_worst_fitness.txt', "w")
+results_by_worst_fitness = open('tests/results/results_by_worst_fitness.txt', "w")
 for i, result in enumerate(sorted_by_worst_fitness):
     results_by_worst_fitness.write(f'{i + 1}- {result.name, result.worst_fitness}\n')
 
-results_by_worst_time = open('tests/collection/results/results_by_worst_time.txt', "w")
+results_by_worst_time = open('tests/results/results_by_worst_time.txt', "w")
 for i, result in enumerate(sorted_by_worst_time):
     results_by_worst_time.write(f'{i + 1}- {result.name, result.worst_time}\n')
